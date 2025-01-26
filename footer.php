0b18f004c891a87dcce8df02b40e5d2db7e5088b
Himmat Shrestha <himmat.shrestha@gmail.com>
2025-01-26 14:29:31 +1100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <p>&copy; 2025 Himmat Shrestha. All rights reserved.</p>
+        <div class="social-links">
+            <a href="#">LinkedIn</a>
+            <a href="#">GitHub</a>
+            <a href="#">Twitter</a>
+        </div>
+    </footer>
