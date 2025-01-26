d3de06a03e8da00b07396c4e21b2c5c47ac51109
Himmat Shrestha <himmat.shrestha@gmail.com>
2025-01-26 17:04:32 +1100

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-        <p>&copy; 2025 Himmat Shrestha. All rights reserved.</p>
-        <div class="social-links">
-            <a href="#">LinkedIn</a>
-            <a href="#">GitHub</a>
-            <a href="#">Twitter</a>
-        </div>
-    </footer>
